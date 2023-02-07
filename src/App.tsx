9b14dc5f4f26244866f75da5c327ab6134722a96
Jonathan Kong <jonathan.kong@zendesk.com>
2023-02-07 13:35:16 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+// import { Routes, Route } from 'react-router-dom'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl mt-10 font-bold text-green-500 text-center'>
+        Medusa
+      </h1>
+      <span>beta</span>
+    </>
+  )
+}
+
+export default App
+
+// <Routes>
+//   <Route path='/' element={<Home />} />
+//   <Route path='/store' element={<About />} />
+//   <Route path='/about' element={<Http />} />
+//   <Route path='/about' element={<RESTApi />} />
+//   <Route path='/about' element={<Kafka />} />
+//   <Route path='/about' element={<grpc />} />
+//   <Route path='/about' element={<graphQL />} />
+// </Routes>
